76e8ebc1a84d0a4f3d0e07bc3082b3bb80400ffd
James Wu <jameswu62@yahoo.co.nz>
2022-12-23 11:13:02 +1300

    add comment

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include "new_blocker.hpp"
 #endif
 
+// newer compiler can use this instead of blocker
 // void *operator new(std::size_t) = delete;
 // void *operator new[](std::size_t) = delete;
 
